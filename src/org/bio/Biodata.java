f526e6951f24b1f7aec02f00119728278dae85d5
rajesh897 <rajsircrr@gmail.com>
2021-12-10 16:57:31 +0530

    my first git programming

---
+++ src/org/bio/Biodata.java
@@ -0,0 +1,26 @@
+package org.bio;
+
+public class Biodata {
+	public static void main(String[] args) {
+		byte age = 22;
+				System.out.println("my age is:"+age);
+				short myheight = 155;
+				System.out.println("my height is :"+myheight);
+				int i = 984555;
+				System.out.println(i);
+				long myphone =985461234715214l;
+				System.out.println(myphone);
+				float sal = 525.2532f;
+				System.out.println(sal);
+				double avg = 252.23125;
+				System.out.println(avg);
+				char gen ='m';
+				System.out.println(gen);
+				
+				
+				
+		
+		
+	}
+
+}
